76719ce9ea602f6853b7db54a364bb7555983222
Lev Livnev <lev@liv.nev.org.uk>
2019-08-26 14:56:43 +0100

    initial

--- src/TestProber.t.sol
+++ src/TestProber.t.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.5.10;
 
 import "ds-test/test.sol";
+import "erc20/erc20.sol"
 
 import "./TestProber.sol";
 
 contract TestProberTest is DSTest {
     TestProber prober;
+    ERC20 public constant usdc = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     function setUp() public {
         prober = new TestProber();
@@ -17,5 +19,9 @@ contract TestProberTest is DSTest {
 
     function test_basic_sanity() public {
         assertTrue(true);
+    }
+
+    function try_usdc_call() public {
+
     }
 }
