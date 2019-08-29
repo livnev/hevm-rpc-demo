ba73ec81d8b10b351b9d63e803af9b6950257344
Lev Livnev <lev@symbolic.partners>
2019-08-29 18:04:00 +0100

    initial commit

--- src/TestProber.t.sol
+++ src/TestProber.t.sol
@@ -1,27 +1,17 @@
 pragma solidity ^0.5.10;
 
 import "ds-test/test.sol";
-import "erc20/erc20.sol"
-
-import "./TestProber.sol";
+import "erc20/erc20.sol";
 
 contract TestProberTest is DSTest {
-    TestProber prober;
-    ERC20 public constant usdc = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    ERC20   public constant usdc = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address public constant binance = 0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8;
 
     function setUp() public {
-        prober = new TestProber();
+        // did not deploy anything to usdc!
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
-
-    function test_basic_sanity() public {
-        assertTrue(true);
-    }
-
-    function try_usdc_call() public {
-
+    function test_binance_balance() public {
+        assertTrue(usdc.balanceOf(binance) > 0);
     }
 }
